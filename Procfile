d17be27e68ff3a47a13987e29a61c42d39b69d92
omagoobscuro <celticfolkmetal@hotmail.com>
2019-05-22 13:58:12 -0300

    deploy heroky

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sindicato.wsgi --log-file -
--- sindicato/settings.py
+++ sindicato/settings.py
@@ -1,6 +1,7 @@
 
 import os
 from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -15,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'https://sindregistra.herokuapp.com/']
 
 
 # Application definition
